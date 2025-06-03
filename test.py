fa85e49380653ccc5d771cdb4b89e3e5ff3106a7
peter-lie <peter.lie14@icloud.com>
2025-06-03 13:39:32 -0700

    Satellite boid pseudocode

--- test.py
+++ test.py
@@ -4,9 +4,23 @@
 
 import numpy as np
 
-print("pi is", np.pi)
+# 3D Boids algorithm - be super cool if applicable on satellites
 
-# New branch differences
-print("tau is", 2 * np.pi)
+# Start with orbital motion
 
-# Caleb says hi
+# Implement collision avoidance - like geese agents
+
+
+# Orbital motion goals:
+    # Start scenario similar to rocket release
+    # Achieve even spacing - phasing
+    # Correct orbit
+    # Collision avoidance
+        # Follow boids algorithms
+        # Sense other boids in close proximity
+        # Can define a view angle in here if we want - but unlimited orientation in space
+        # Steer away from other boids in close proximity
+
+    # Avoid a rocket
+    # Track total delta-v if possible
+
